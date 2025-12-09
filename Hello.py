e6b3054821aa7c655b2e4e0df4581d015966683c
irpolly <i.r.polly@gmail.com>
2025-12-09 21:27:23 +0200

    modified:   Hello.py

--- Hello.py
+++ Hello.py
@@ -1,3 +1,4 @@
 # Hello Py
-whoAreYou = input("Hello, Python!\nWhat is your name? ")
+whoAreYou = input("Hello, Python!\nWhat is your Name & Nickname\n/G\na\nm\ne\nr\nt\na\ng\n? ")
 print(f"Hello, {whoAreYou} in Python!")
+print(2*17)
